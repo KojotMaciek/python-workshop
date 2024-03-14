552ff42f4f63cb971489d2abbff27a637e9f5709
Maciej Wilkosz <maciej.wilkosz@motorolasolutions.com>
2024-03-14 11:08:54 +0100

    Mars Rover clean code and unit tests

--- tasks/mars_rover_technical_challenge.py
+++ tasks/mars_rover_technical_challenge.py
@@ -18,18 +18,12 @@ class Rover:
     def move(self, input):
         move = {"N": (0, 1), "S": (0, -1), "W": (-1, 0), "E": (1, 0)}
         left = {"N": "W", "S": "E", "W": "S", "E": "N"}
+        right = {"N": "E", "S": "W", "W": "N", "E": "S"}
         for cmd in [*input]:
             if cmd == "L":
                 self.d = left[self.d]
             elif cmd == "R":
-                if self.d == "E":
-                    self.d = "S"
-                elif self.d == "S":
-                    self.d = "W"
-                elif self.d == "W":
-                    self.d = "N"
-                else:
-                    self.d = "E"
+                self.d = right[self.d]
             elif cmd == "M":
                 newx = self.x + move[self.d][0]
                 newy = self.y + move[self.d][1]
@@ -37,75 +31,3 @@ class Rover:
                     self.x = newx
                 if newy >= 0 and newy <= self.my:
                     self.y = newy
-
-"""                 if self.d == "W" and self.x > 0:
-                    self.x -= 1
-                elif self.d == "E" and self.x < self.mx:
-                    self.x += 1
-                elif self.d == "N" and self.y < self.my:
-                    self.y += 1
-                elif self.d == "S" and self.y > 0:
-                    self.y -= 1 """
-
-machine_1 = Rover(5, 6, "N")
-machine_2 = Rover(0, 0, "W")
-machine_3 = Rover(5, 6, "N")
-machine_4 = Rover(5, 5, "E", 5, 5)
-
-""" def object_play():
-    print(machine_1.whereAmI())
-    print(machine_1.d) Hack
-
-    print(machine_1)
-    print(machine_1 == machine_1)
-    print(machine_2 == machine_1)
-    print(machine_3 == machine_1)
- """
-def test():
-    machine_1.move("L")
-    assert machine_1 == Rover(5, 6, "W")
-    machine_1.move("L")
-    assert machine_1 == Rover(5, 6, "S")
-    machine_1.move("L")
-    machine_1.move("L")
-    assert machine_1 == Rover(5, 6, "N")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "E")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "S")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "W")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "N")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "E")
-    machine_1.move("R")
-    machine_1.move("R")
-    assert machine_1 == Rover(5, 6, "W")
-    machine_1.move("W")
-    assert machine_1 == Rover(5, 6, "W")
-    machine_1.move("M")
-    assert machine_1 == Rover(4, 6, "W")
-    machine_1.move("R")
-    machine_1.move("R")
-    machine_1.move("M")
-    assert machine_1 == Rover(5, 6, "E")
-    machine_2.move("M")
-    assert machine_2 == Rover(0, 0, "W")
-    machine_4.move("M")
-    assert machine_4 == Rover(5, 5, "E")
-    machine_4.move("L")
-    machine_4.move("M")
-    assert machine_4 == Rover(5, 5, "N")
-    machine_2.move("L")
-    machine_2.move("M")
-    assert machine_2 == Rover(0, 0, "S")
-    machine_5 = Rover(1, 2, "N", 5, 5)
-    machine_5.move("LMLMLMLMM")
-    assert machine_5 == Rover(1, 3, "N")
-    machine_6 = Rover(3, 3, "E", 5, 5)
-    machine_6.move("MMRMMRMRRM")
-    assert machine_6 == Rover(5, 1, "E")
-
-if __name__ == '__main__':
-    test()
---
+++ tasks/test_mars_rover_technical_challenge.py
@@ -0,0 +1,90 @@
+import unittest
+from mars_rover_technical_challenge import Rover
+
+class TestMarsRover(unittest.TestCase):
+    machine_4 = Rover(5, 5, "E", 5, 5)
+    machine_5 = Rover(1, 2, "N", 5, 5)
+    machine_6 = Rover(3, 3, "E", 5, 5)
+
+    def test_whereAmI(self):
+        machine_1 = Rover(5, 6, "N")
+        assert machine_1.whereAmI() == (5, 6, "N")
+
+    def test_whereAmICompareTrue(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_3 = Rover(5, 6, "N")
+        assert machine_1.whereAmI() == machine_3.whereAmI()
+
+    def test_whereAmICompareFalse(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_2 = Rover(0, 0, "W")
+        assert machine_1.whereAmI() != machine_2.whereAmI()
+    
+    def test_turnLeft(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_1.move("L")
+        assert machine_1 == Rover(5, 6, "W")
+
+    def test_turnRight(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_1.move("R")
+        assert machine_1 == Rover(5, 6, "E")
+
+    def test_turnLeftTwoTimes(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_1.move("L")
+        machine_1.move("L")
+        assert machine_1 == Rover(5, 6, "S")
+
+    def test_turnRightTwoTimes(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_1.move("R")
+        machine_1.move("R")
+        assert machine_1 == Rover(5, 6, "S")
+
+    def test_turnLeftTwoTimes(self):
+        machine_1 = Rover(5, 6, "E")
+        machine_1.move("R")
+        machine_1.move("R")
+        assert machine_1 == Rover(5, 6, "W")
+
+    def test_turnRightTwoTimes(self):
+        machine_1 = Rover(5, 6, "N")
+        machine_1.move("R")
+        machine_1.move("R")
+        assert machine_1 == Rover(5, 6, "S")
+
+    def test_turnInBothDirections(self):
+        machine_1 = Rover(5, 6, "E")
+        machine_1.move("LRRLR")
+        assert machine_1 == Rover(5, 6, "S")
+
+    def test_moveHorizontaly(self):
+        machine_1 = Rover(5, 6, "E")
+        machine_1.move("M")
+        assert machine_1 == Rover(6, 6, "E")
+
+    def test_moveVerticaly(self):
+        machine_1 = Rover(5, 6, "E")
+        machine_1.move("M")
+        assert machine_1 == Rover(6, 6, "E")
+
+    def test_horizontalMapLimits(self):
+        machine_2 = Rover(0, 0, "W")
+        machine_2.move("M")
+        assert machine_2 == Rover(0, 0, "W")
+
+    def test_verticalMapLimits(self):
+        machine_4 = Rover(5, 5, "E", 5, 5)
+        machine_4.move("M")
+        assert machine_4 == Rover(5, 5, "E")
+
+    def test_combinedMovesRover1(self):
+        machine_5 = Rover(1, 2, "N", 5, 5)
+        machine_5.move("LMLMLMLMM")
+        assert machine_5 == Rover(1, 3, "N")
+
+    def test_combinedMovesRover2(self):
+        machine_6 = Rover(3, 3, "E", 5, 5)
+        machine_6.move("MMRMMRMRRM")
+        assert machine_6 == Rover(5, 1, "E")
