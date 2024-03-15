a39246bc7b37780d946fdcd01f24c56ac4d0ca76
Maciej Wilkosz <maciej.wilkosz@motorolasolutions.com>
2024-03-15 12:51:21 +0100

    Test messages added - in case of fail

--- tasks/test_game_of_life.py
+++ tasks/test_game_of_life.py
@@ -4,22 +4,27 @@ import game_of_life
 class TestGameOfLife(unittest.TestCase):
     def test_classExist(self):
         l = game_of_life.Life("0 0")
+        print('Testing basic functionality')
 
     def test_getGrid(self):
         l = game_of_life.Life("0 0")
+        print('Testing \"0 0\"')
         assert l.getGrid() == "0 0"
 
     def test_generateLife(self):
         l = game_of_life.Life("0 0")
         l.nextGeneration()
+        print('Testing generating life for grid \"0 0\"')
         assert l.getGrid() == "0 0"
 
     def test_lastCellDies(self):
         l = game_of_life.Life("3 3\n...\n.*.\n...")
         l.nextGeneration()
+        print('Testing \"Last Cell Dies\" for grid \"3 3\"')
         assert l.getGrid() == "3 3\n...\n...\n..."
 
     def test_lastCellLives(self):
         l = game_of_life.Life("3 5\n.....\n.***.\n.....")
         l.nextGeneration()
+        print('Testing \"Last Cell lives\" for grid \"3 5\"')
         assert l.getGrid() == "3 5\n.....\n..*..\n....."
