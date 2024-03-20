80c40eb7e608e59abe16b2d1ba76aba168123727
Maciej Wilkosz <maciej.wilkosz@motorolasolutions.com>
2024-03-20 10:45:35 +0100

    Alone cells die

--- tasks/inProgress-iteration.py
+++ tasks/inProgress-iteration.py
@@ -1,14 +1,9 @@
-# Ogolnie to z takiego stringa:
-# "3 5\n.....\n.*.*.\n....."
-# Powinien wyjsc taki:
-# "3 5\n.....\n.....\n....."
-
-a = "3 5\n.....\n.*.*.\n.....".split()
+a = "3 5\n.....\n.*.*.\n.....".split("\n")
 print(a)
-print(len(a[1]))
-for i in range(len(a[2])):
-    print(a[3][i:i+3].replace(".*.", "..."))
+for i, s in enumerate(a):
+    a[i] = s.replace('*.*', '...')
 
-# Umiem to w locie zmienic, ale juz zastapic nie umiem
 print(a)
 
+b = '\n'.join(a)
+print(b)
